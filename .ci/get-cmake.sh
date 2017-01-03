218f5bd8592ce093a9a3d5cc3b6915506cd24942
Albert Cervin <albert@acervin.com>
2017-01-03 11:03:33 +0100

    Update get-cmake.sh

--- .ci/get-cmake.sh
+++ .ci/get-cmake.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
 wget https://cmake.org/files/v3.7/cmake-3.7.1-Linux-x86_64.sh
-./cmake-3.7.1-Linux-x86_64.sh --prefix=/usr/local
+sudo ./cmake-3.7.1-Linux-x86_64.sh --prefix=/usr/local
