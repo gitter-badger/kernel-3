2d1606ec3b95635d19431d3c6f21b38cdbdb6e29
Albert Cervin <albert@acervin.com>
2017-01-04 08:57:40 +0100

    Update get-cmake.sh

--- .ci/get-cmake.sh
+++ .ci/get-cmake.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 wget https://cmake.org/files/v3.7/cmake-3.7.1-Linux-x86_64.sh
-echo $PWD
-sudo echo $PWD
-ls -la ./
 chmod +x cmake-3.7.1-Linux-x86_64.sh
-sudo ./cmake-3.7.1-Linux-x86_64.sh --prefix=/usr/local
+sudo ./cmake-3.7.1-Linux-x86_64.sh --prefix=/usr/local --skip-license
